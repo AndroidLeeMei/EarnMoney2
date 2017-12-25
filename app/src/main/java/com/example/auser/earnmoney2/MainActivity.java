19deba2b5da8bbb208a4b12bfb964f2987d09f65
LeeMei <boubouian@yahoo.com.tw>
2017-12-25 16:35:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/earnmoney2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.auser.earnmoney2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-6198110215236018~4108016653");
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
